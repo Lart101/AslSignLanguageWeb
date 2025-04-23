3692b4645fef7f7b005a5c55c7a18e0a6d79754f
Lart <yeojlacrete@gmai.com>
2025-04-23 23:23:27 +0800

    Update GESTURE_MODEL_URL to point to the latest model version

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
-export const GESTURE_MODEL_URL = "https://rgxalrnmnlbmskupyhcm.supabase.co/storage/v1/object/public/signlanguage//Asl14000imagePART2.task";
 
+export const GESTURE_MODEL_URL = "https://rgxalrnmnlbmskupyhcm.supabase.co/storage/v1/object/public/signlanguage//Asl14000imagePART3.task";
